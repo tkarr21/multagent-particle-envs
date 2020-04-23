4c494882accf3ffe0264d403a15b7cd6321de039
Tyler Karren <tkarren22@gmail.com>
2020-04-22 18:00:06 -0600

    render fix, one tagger

--- multiagent/rendering.py
+++ multiagent/rendering.py
@@ -110,15 +110,15 @@ class Viewer(object):
         if return_rgb_array:
             buffer = pyglet.image.get_buffer_manager().get_color_buffer()
             image_data = buffer.get_image_data()
-            arr = np.fromstring(image_data.get_data('RGBA', image_data.width * 4), dtype=np.uint8, sep='')
+            arr = np.fromstring(image_data.data, dtype=np.uint8, sep='')
             # In https://github.com/openai/gym-http-api/issues/2, we
             # discovered that someone using Xmonad on Arch was having
             # a window of size 598 x 398, though a 600 x 400 window
             # was requested. (Guess Xmonad was preserving a pixel for
             # the boundary.) So we use the buffer height/width rather
             # than the requested one.
-            arr = arr.reshape(buffer.height, buffer.width, 3)
-            arr = arr[::-1,:,0:2]
+            arr = arr.reshape(buffer.height, buffer.width, 4)
+            arr = arr[::-1,:,0:3]
         self.window.flip()
         self.onetime_geoms = []
         return arr
@@ -152,9 +152,9 @@ class Viewer(object):
         self.window.flip()
         image_data = pyglet.image.get_buffer_manager().get_color_buffer().get_image_data()
         self.window.flip()
-        arr = np.fromstring(image_data.get_data('RGBA', image_data.width * 4), dtype=np.uint8, sep='')
-        arr = arr.reshape(self.height, self.width, 3)
-        return arr[::-1,:,0:2]
+        arr = np.fromstring(image_data.data, dtype=np.uint8, sep='')
+        arr = arr.reshape(self.height, self.width, 4)
+        return arr[::-1,:,0:3]
 
 def _add_attrs(geom, attrs):
     if "color" in attrs:
